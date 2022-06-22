195a4e0bfdaef83b97c8caa9e1b0425c0351dc3e
hm1717 <steelcooker17@gmail.com>
2022-06-23 05:37:14 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customer from './components/Customer.js'
-import Paper from '@material-ui/core/Paper'
-import Table from '@material-ui/core/Table'
-import TableHead from '@material-ui/core/TableHead'
-import TableBody from '@material-ui/core/TableBody'
-import TableRow from '@material-ui/core/TableRow'
-import TableCell from '@material-ui/core/TableCell'
+import Customer from './components/Customer.js';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
+
+/*
+LIFE CYCLE
+
+1. constructor()
+2. componentWillMount()
+3. render()
+4. componentDidMount()
+*/
+/*
+
+props or state => shouldcomponentUpdate()
+
+*/
+
 
 const styles = theme => ({
   root: {
@@ -18,8 +34,11 @@ const styles = theme => ({
   },
   table:{
     minWidth: 1080
+  },
+  profress: {
+    margin: theme.spacing.unit * 2
   }
-})
+});
 
 
 
@@ -27,9 +46,11 @@ const styles = theme => ({
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -39,6 +60,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+  
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render(){
@@ -60,7 +86,13 @@ class App extends Component {
           <TableBody>
             {this.state.customers ? this.state.customers.map(c=>{ 
               return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)
-            }) : ""}
+            }) : 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
